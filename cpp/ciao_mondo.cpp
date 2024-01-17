995e3c54d57b6dd9db43a309776404ab7d19aac2
Alice Orlandini <62644820+AliceOrlandini@users.noreply.github.com>
2024-01-17 12:24:54 +0100

    Update endl statement in ciao_mondo.cpp

--- cpp/ciao_mondo.cpp
+++ cpp/ciao_mondo.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
   // stampa "Hello World!" tramite cout contenuto nella libreria iostream
-  // terminando con un a capo tramite endl
+  // terminando con un a capo tramite l'istruzione endl
   cout << "Hello World!" << endl;
   return 0;
 }
